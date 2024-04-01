5751134c6a2150ccce9a0584aae2c0402eb11606
maskikuy <kproject305@gmail.com>
2024-04-01 15:30:28 +0700

    masukan alamat mahasiswa

--- Activitystrict2_0060/Activitystrict2_0060.cpp
+++ Activitystrict2_0060/Activitystrict2_0060.cpp
@@ -18,3 +18,12 @@ int main() {
     cin >> mhs.nim;
     cout << "Nama Mahasiswa : ";
     cin >> mhs.nama;
+
+    cout << "Alamat Mahasiswa :" << endl;
+    cout << "\t Nama Desa : ";
+    cin >> mhs.alamat.desa;
+    cout << "\t Nama Kota : ";
+    cin >> mhs.alamat.kota;
+
+    cout << "Umur Mahasiswa : ";
+    cin >> mhs.umur;
